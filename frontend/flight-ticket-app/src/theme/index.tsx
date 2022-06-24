1ee7d88bbe4f68d589d71476722ee3a07b3402d6
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-06-24 11:25:46 -0300

    💄 added theme provider

---
+++ frontend/flight-ticket-app/src/theme/index.tsx
@@ -0,0 +1,32 @@
+import { DefaultTheme, ThemeProvider } from 'styled-components';
+
+const Theme: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+
+	const config: DefaultTheme = {
+		colors: {
+			text: {
+				primary: '#FFFFFF',
+				secondary: '#A5A8B0',
+				tertiary: '#000000'
+			},
+
+			main: {
+				primary: '#45EA69'
+			},
+
+			background: {
+				primary: '#1D1D1F',
+				secondary: '#313133'
+			}
+		}
+	};
+
+	return(
+		<ThemeProvider theme={config}>
+			{children}
+		</ThemeProvider>
+	);
+};
+
+export default Theme;
+
---
+++ frontend/flight-ticket-app/src/theme/styles.d.ts
@@ -0,0 +1,22 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      main: {
+        primary: string;
+      }
+
+      text: {
+        primary: string;
+        secondary: string;
+        tertiary: string;
+      }
+
+      background: {
+        primary: string;
+        secondary: string;
+      }
+    }
+  }
+}

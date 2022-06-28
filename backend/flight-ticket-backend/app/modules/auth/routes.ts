ffe4186ee21412586312c896e24a448dd4d46bf6
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-06-28 17:10:25 -0300

    🔧 added configuration for social authentication

--- backend/flight-ticket-backend/app/modules/auth/routes.ts
+++ backend/flight-ticket-backend/app/modules/auth/routes.ts
@@ -3,6 +3,10 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.group(() => {
     Route.post('/sign-in', 'AuthController.authenticate')
+    Route.get('/sign-in/google', 'AuthController.authenticateWithGoogle')
+    Route.get('/sign-in/google/callback', 'AuthController.authenticateWithGoogleCallback')
+    Route.get('/sign-in/twitter', 'AuthController.authenticateWithTwitter')
+    Route.get('/sign-in/twitter/callback', 'AuthController.authenticateWithTwitterCallback')
     Route.post('/sign-up', 'AuthController.store')
   }).prefix('/v1')
 }).prefix('/api')
